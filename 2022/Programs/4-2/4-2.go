ff4fe4855c435ad49f1e33ec9d2aa3fa94dfb936
Lewin Kelly <KEllyLewin2016@Gmail.com>
2022-12-09 04:06:45 +0000

    Solution for 4-2

---
+++ 2022/Programs/4-2/4-2.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+	"strconv"
+)
+
+func main() {
+	file, err := os.Open("../../Inputs/4")
+	if err != nil {
+		fmt.Println("[error]:", err)
+	}
+	defer file.Close()
+	fileScanner := bufio.NewScanner(file)
+	fileScanner.Split(bufio.ScanLines)
+
+	sum := 0
+
+	for fileScanner.Scan() {
+		arr := []string(strings.Split(fileScanner.Text(), ","))
+		arr0 := strings.Split(arr[0], "-")
+		arr1 := strings.Split(arr[1], "-")
+
+		a1, _ := strconv.Atoi(arr0[0])
+		a2, _ := strconv.Atoi(arr0[1])
+		b1, _ := strconv.Atoi(arr1[0])
+		b2, _ := strconv.Atoi(arr1[1])
+
+		if a1 <= b1 && a2 >= b1 || b1 <= a1 && b2 >= a1 {
+			sum++
+		}
+	}
+	fmt.Println(sum) // 843
+}
