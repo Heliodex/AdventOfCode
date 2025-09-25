44b51824e92a3f5bcf03190cade7e7b0b709d14a
Lewin Kelly <heli@odex.cf>
2025-09-25 09:33:04 +0100

    Add 2015 solution 1-2 in same file as 1-1

---
+++ 2015/Programs/day1.java
@@ -0,0 +1,41 @@
+package Programs;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
+public class day1 {
+	final String path = "./Inputs/1";
+
+	int part1(String input) {
+		var floor = 0;
+		for (char c : input.toCharArray())
+			floor += switch (c) {
+				case '(' -> 1;
+				case ')' -> -1;
+				default -> 0;
+			};
+		return floor;
+	}
+
+	int part2(String input) {
+		var floor = 0;
+		var i = 0;
+		for (; i < input.length(); i++) {
+			floor += switch (input.charAt(i)) {
+				case '(' -> 1;
+				case ')' -> -1;
+				default -> 0;
+			};
+			if (floor == -1)
+				return i + 1;
+		}
+		return i;
+	}
+
+	void main() throws IOException {
+		var content = Files.readString(new File(path).toPath());
+		IO.println(part1(content));
+		IO.println(part2(content));
+	}
+}
