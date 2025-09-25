61a8b237a13a2a5886adc9e05beb871e8dd474c7
Lewin Kelly <heli@odex.cf>
2025-09-25 10:37:53 +0100

    Solution for 2015 day 4

---
+++ 2015/Programs/day4.java
@@ -0,0 +1,28 @@
+package Programs;
+
+import java.io.File;
+import java.io.IOException;
+import java.math.BigInteger;
+import java.nio.file.Files;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class day4 {
+	final String path = "./Inputs/4";
+
+	int find(String input, int zeros) throws NoSuchAlgorithmException {
+		var md = MessageDigest.getInstance("MD5");
+		for (var num = 0;; num++) {
+			md.update((input + num).getBytes());
+
+			if (new BigInteger(1, md.digest()).bitLength() <= (32 - zeros) * 4)
+				return num;
+		}
+	}
+
+	void main() throws IOException, NoSuchAlgorithmException {
+		var content = Files.readString(new File(path).toPath());
+		IO.println(find(content, 5));
+		IO.println(find(content, 6));
+	}
+}
